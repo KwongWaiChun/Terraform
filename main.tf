6cf15fc99d8d243a5dfa4cdad8e8f03e1bd78b17
KwongWaiChun <113015450+KwongWaiChun@users.noreply.github.com>
2024-04-13 16:41:28 +0000

    Changes

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudformation_stack" "Terraform-cloudformation-stack" {
   name = "Terraform-cloudformation-stack"
-  template_body = file("AWS-Terraform-cloudformation.yaml")
+  template_body = file("AWS-Terraform-Cloudformation.yaml")
 }
