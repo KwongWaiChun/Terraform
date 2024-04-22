07f3007bca7f10857141465d63b0ef2aae4fb9fd
loner1007 <131882315+loner1007@users.noreply.github.com>
2024-04-22 09:24:28 +0000

    changes

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-/**
-* Copyright 2024 Google LLC
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-# [START gke_quickstart_autopilot_app]
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
@@ -137,4 +120,3 @@ resource "time_sleep" "wait_service_cleanup" {
 
   destroy_duration = "180s"
 }
-# [END gke_quickstart_autopilot_app]
--- vpc.tf
+++ vpc.tf
@@ -1,20 +1,3 @@
-/**
-* Copyright 2024 Google LLC
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-# [START gke_quickstart_autopilot_cluster]
 resource "google_compute_network" "default" {
   name = "example-network"
 
@@ -63,4 +46,3 @@ resource "google_container_cluster" "default" {
   # accidentally delete this instance by use of Terraform.
   deletion_protection = false
 }
-# [END gke_quickstart_autopilot_cluster]
