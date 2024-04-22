1be0d8e678d3712ab1e33372c135d3e29913be7d
loner1007 <131882315+loner1007@users.noreply.github.com>
2024-04-22 09:27:33 +0000

    test

--- providers.tf
+++ providers.tf
@@ -1,10 +1,4 @@
 # https://www.terraform.io/language/settings/backends/gcs
 terraform {
   backend "gcs" {}
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.0.0"
-    }
-  }
 }
