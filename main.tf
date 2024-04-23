83dd5bd37970290d3e87b221796010436e142b04
loner1007 <131882315+loner1007@users.noreply.github.com>
2024-04-23 22:43:26 +0000

    changes

--- main.tf
+++ main.tf
@@ -1,122 +1,89 @@
+variable "region" {
+  default = "asia-east1" // Set as per your nearest location or preference 
+}
 
+variable "location" {
+  default = "asia-east1-b"  // Set as per your nearest location or preference 
+}
 
-# Create a VPC network
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-vpc-network"
+variable "network_name" {
+  default = "tf-gke-k8s"
+}
+
+variable "image" {
+  default = "kwongwaichun/fyp" // Your web image
+}
+
+provider "google" {
+  region = var.region
+}
+
+resource "google_compute_network" "default" {
+  name                    = var.network_name
   auto_create_subnetworks = false
 }
 
-# Create a subnet within the VPC
-resource "google_compute_subnetwork" "subnet" {
-  name          = "my-subnet"
-  network       = google_compute_network.vpc_network.self_link
-  ip_cidr_range = "10.0.0.0/24"
-
-  region                     = "us-central1"
-  private_ip_google_access   = true
-  secondary_ip_range {
-    range_name    = "my-secondary-range"
-    ip_cidr_range = "10.0.1.0/24"
-  }
+resource "google_compute_subnetwork" "default" {
+  name                     = var.network_name
+  ip_cidr_range            = "10.140.0.0/20"  // Change as per your region/zone
+  network                  = google_compute_network.default.self_link
+  region                   = var.region
+  private_ip_google_access = true
 }
 
-# Create a GKE cluster
-resource "google_container_cluster" "gke_cluster" {
-  name               = "my-gke-cluster"
-  location           = "us-central1"
-  remove_default_node_pool = true
-
-  master_auth {
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-
-  initial_node_count = 1
-
-  node_pool {
-    name         = "default-pool"
-    machine_type = "n1-standard-2"
-    disk_size_gb = 100
-    node_count   = 1
-    autoscaling {
-      min_node_count = 1
-      max_node_count = 5
-    }
-    management {
-      auto_repair  = true
-      auto_upgrade = true
-    }
-  }
-
-  network_config {
-    subnetwork = google_compute_subnetwork.subnet.self_link
-    ip_allocation_policy {
-      cluster_secondary_range_name = google_compute_subnetwork.subnet.secondary_ip_range.0.range_name
-    }
-  }
+data "google_client_config" "current" {
 }
 
-# Create a regional GKE node pool
-resource "google_container_node_pool" "regional_node_pool" {
-  name       = "regional-pool"
-  location   = "us-central1"
-  cluster    = google_container_cluster.gke_cluster.name
-  node_count = 1
+data "google_container_engine_versions" "default" {
+  location = var.location
+}
+
+resource "google_container_cluster" "default" {
+  name               = var.network_name
+  location           = var.location
+  initial_node_count = 3
+  min_master_version = data.google_container_engine_versions.default.latest_master_version
+  network            = google_compute_subnetwork.default.name
+  subnetwork         = google_compute_subnetwork.default.name
+
+  // Use legacy ABAC until these issues are resolved:
+  // https://github.com/mcuadros/terraform-provider-helm/issues/56
+  // https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
+  enable_legacy_abac = true
+
+  // Add load balancer and auto scaling configurations
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+    disk_size_gb = 30
+    image_type   = "COS"
+    image        = var.image
+  }
+
+  // Add auto scaling configuration
+  enable_autoscaling = true
   autoscaling {
     min_node_count = 1
     max_node_count = 5
   }
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
 }
 
-# Create a regional forwarding rule for load balancing
-resource "google_compute_forwarding_rule" "lb_forwarding_rule" {
-  name             = "lb-forwarding-rule"
-  region           = "us-central1"
-  ip_protocol      = "TCP"
-  port_range       = "80-8080"
-  target           = google_container_node_pool.regional_node_pool.self_link
-  load_balancing_scheme = "EXTERNAL"
+output "network" {
+  value = google_compute_subnetwork.default.network
 }
 
-# Create a health check for load balancing
-resource "google_compute_health_check" "lb_health_check" {
-  name                = "lb-health-check"
-  check_interval_sec  = 10
-  timeout_sec         = 5
-  tcp_health_check {
-    port_specification = "USE_SERVING_PORT"
-  }
+output "subnetwork_name" {
+  value = google_compute_subnetwork.default.name
 }
 
-# Attach the health check to the load balancer
-resource "google_compute_backend_service" "lb_backend_service" {
-  name            = "lb-backend-service"
-  health_checks   = [google_compute_health_check.lb_health_check.self_link]
-  backend {
-    group = google_container_node_pool.regional_node_pool.instance_group_urls[0]
-  }
+output "cluster_name" {
+  value = google_container_cluster.default.name
 }
 
-# Create a URL map for load balancing
-resource "google_compute_url_map" "lb_url_map" {
-  name             = "lb-url-map"
-  default_service  = google_compute_backend_service.lb_backend_service.self_link
+output "cluster_region" {
+  value = var.region
 }
 
-# Create a target HTTP proxy for load balancing
-resource "google_compute_target_http_proxy" "lb_target_http_proxy" {
-  name    = "lb-target-http-proxy"
-  url_map = google_compute_url_map.lb_url_map.self_link
-}
-
-# Create a global forwarding rule for load balancing
-resource "google_compute_global_forwarding_rule" "lb_global_forwarding_rule" {
-  name        = "lb-global-forwarding-rule"
-  target      = google_compute_target_http_proxy.lb_target_http_proxy.self_link
-  port_range  = "80-8080"
+output "cluster_location" {
+  value = google_container_cluster.default.location
 }
