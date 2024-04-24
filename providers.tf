fbceb0fc16b695f88c46cfd5b4b882ca53a0d81a
loner1007 <131882315+loner1007@users.noreply.github.com>
2024-04-24 09:09:54 +0000

    changes

--- providers.tf
+++ providers.tf
@@ -2,3 +2,29 @@
 terraform {
   backend "gcs" {}
 }
+
+terraform {
+ required_providers {
+   google = {
+     source = "hashicorp/google"
+     version = "5.3.0"
+   }
+ }
+}
+
+provider "google" {
+ # Configuration options
+}
+
+terraform {
+ required_providers {
+   kubernetes = {
+     source = "hashicorp/kubernetes"
+     version = "2.23.0"
+   }
+ }
+}
+
+provider "kubernetes" {
+ # Configuration options
+}
