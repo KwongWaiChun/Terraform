a9bd41e0ca5be63b26b63a36110464a4799aafa9
loner1007 <131882315+loner1007@users.noreply.github.com>
2024-04-24 05:48:30 +0000

    changes

--- k8s.tf
+++ k8s.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      version = "~> 1.10.0"
-    }
-  }
-}
-
 provider "kubernetes" {
+  version = "~> 1.10.0"
   host    = google_container_cluster.default.endpoint
   token   = data.google_client_config.current.access_token
   client_certificate = base64decode(
