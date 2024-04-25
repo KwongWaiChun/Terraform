70c8e7a56913253a79f6273b6558ce72ca242bd1
loner1007 <131882315+loner1007@users.noreply.github.com>
2024-04-25 20:37:51 +0000

    changes

--- main.tf
+++ main.tf
@@ -19,23 +19,16 @@ resource "google_compute_network" "default" {
   auto_create_subnetworks = false
 }
 
+resource "google_compute_address" "flask_app_ip" {
+  name = "flask-app-ip"
+}
+
 resource "google_compute_subnetwork" "default" {
   name                     = var.network_name
   ip_cidr_range            = "10.140.0.0/20"  // Change as per your region/zone
   network                  = google_compute_network.default.self_link
   region                   = var.region
   private_ip_google_access = true
-}
-
-resource "google_service_networking_connection" "vpc_peering" {
-  provider = google-beta
-  network                 = google_compute_network.default.name
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges =       [google_compute_global_address.peering_address.name]
-}
-
-resource "google_compute_global_address" "flask_app_ip" {
-  name = "flask-app-ip"
 }
 
 data "google_client_config" "current" {
