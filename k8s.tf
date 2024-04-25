bafb5114775545bdcff6dc9972db3a550d90f96d
loner1007 <131882315+loner1007@users.noreply.github.com>
2024-04-25 16:12:41 +0000

    changes

--- k8s.tf
+++ k8s.tf
@@ -17,43 +17,39 @@ resource "kubernetes_namespace" "staging" {
   }
 }
 
+resource "kubernetes_secret" "tls_cred" {
+  metadata {
+            name = "tls-cred"
+            namespace = kubernetes_namespace.staging.metadata[0].name
+          }
+  data = {
+            "tls.crt" = file("${path.root}/.tls/revevellidan.com_ssl_certificate.cer")
+            "tls.key" = file("${path.root}/.tls/_.revevellidan.com_private_key.key")
+        }
+type = "kubernetes.io/tls"
+}
+
+resource "kubernetes_ingress" "flask-ingress" {
+  metadata {
+    name = "flask-ingress"
+    annotations = {
+      "kubernetes.io/ingress.global-static-ip-name" = "flask-app-ip"
+    }
+  }
+  spec {
+    tls {
+      secret_name = kubernetes_secret.tls_cred.metadata[0].name
+    }
+    backend {
+      service_name = kubernetes_service.nginx.metadata[0].name
+      service_port = 443
+    }
+  }
+}
+
 resource "google_compute_address" "default" {
   name   = var.network_name
   region = var.region
-}
-
-resource "tls_private_key" "nginx" {
-  algorithm = "RSA"
-}
-
-resource "tls_locally_signed_cert" "nginx" {
-  locals {
-    common_name = "k8s.tf"
-  }
-
-  cert_request {
-    private_key_pem = tls_private_key.nginx.private_key_pem
-
-    dns_names = [
-      "k8s.tf",
-    ]
-  }
-
-  validity_period_hours = 12
-}
-
-resource "kubernetes_secret" "tls" {
-  metadata {
-    name      = "tls-secret"
-    namespace = kubernetes_namespace.staging.metadata[0].name
-  }
-
-  data = {
-    "tls.crt" = tls_locally_signed_cert.nginx.cert_pem
-    "tls.key" = tls_private_key.nginx.private_key_pem
-  }
-
-  type = "kubernetes.io/tls"
 }
 
 resource "kubernetes_service" "nginx" {
@@ -75,47 +71,11 @@ resource "kubernetes_service" "nginx" {
       target_port = 80
     }
 
-    type = "LoadBalancer"
+    type             = "LoadBalancer"
     load_balancer_ip = google_compute_address.default.address
   }
 }
 
-resource "kubernetes_ingress" "nginx" {
-  metadata {
-    namespace = kubernetes_namespace.staging.metadata[0].name
-    name      = "nginx"
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-      "nginx.ingress.kubernetes.io/ssl-redirect" = "true"
-      "nginx.ingress.kubernetes.io/ssl-passthrough" = "true"
-    }
-  }
-
-  spec {
-    tls {
-      hosts = [
-        "k8s.tf",
-      ]
-      secret_name = kubernetes_secret.tls.metadata[0].name
-    }
-
-    rule {
-      http {
-        path {
-          path = "/"
-          backend {
-            service {
-              name = kubernetes_service.nginx.metadata[0].name
-              port {
-                number = 443
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
 
 resource "kubernetes_deployment" "nginx" {
   metadata {
@@ -159,6 +119,7 @@ resource "kubernetes_deployment" "nginx" {
     }
   }
 }
+
 
 output "load-balancer-ip" {
   value = google_compute_address.default.address
