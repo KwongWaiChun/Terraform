252404325ce16cbe70a0e09d80cc0691e48f4461
loner1007 <131882315+loner1007@users.noreply.github.com>
2024-04-28 16:42:17 +0000

    changes

---
+++ v2/k8s.tf
@@ -0,0 +1,107 @@
+provider "kubernetes" {
+  version = "~> 1.10.0"
+  host    = google_container_cluster.default.endpoint
+  token   = data.google_client_config.current.access_token
+  client_certificate = base64decode(
+    google_container_cluster.default.master_auth[0].client_certificate,
+  )
+  client_key = base64decode(google_container_cluster.default.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+  )
+}
+
+resource "kubernetes_namespace" "staging" {
+  metadata {
+    name = "staging"
+  }
+}
+
+resource "google_compute_address" "default" {
+  name   = var.network_name
+  region = var.region
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    namespace = kubernetes_namespace.staging.metadata[0].name
+    name      = "nginx"
+  }
+
+  spec {
+    selector = {
+      run = "nginx"
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 80
+    }
+
+    type             = "LoadBalancer"
+    load_balancer_ip = google_compute_address.default.address
+  }
+}
+
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.staging.metadata[0].name
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        run = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          run = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "kwongwaichun/fyp:latest"
+
+          resources {
+            limits {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_secret" "tls_cred" {
+  metadata {
+            name = "tls-cred"
+            namespace = kubernetes_namespace.staging.metadata[0].name
+          }
+  data = {
+            "tls.crt" = file("tls.crt")
+            "tls.key" = file("tls.key")
+        }
+type = "kubernetes.io/tls"
+}
+
+output "load-balancer-ip" {
+  value = google_compute_address.default.address
+}
---
+++ v2/main.tf
@@ -0,0 +1,100 @@
+variable "region" {
+  default = "asia-east1" // Set as per your nearest location or preference 
+}
+
+variable "location" {
+  default = "asia-east1-b"  // Set as per your nearest location or preference 
+}
+
+variable "network_name" {
+  default = "tf-gke-k8s"
+}
+
+provider "google" {
+  region = var.region
+}
+
+resource "google_compute_network" "default" {
+  name                    = var.network_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_address" "flask_app_ip" {
+  name = "flask-app-ip"
+}
+
+resource "google_compute_subnetwork" "default" {
+  name                     = var.network_name
+  ip_cidr_range            = "10.140.0.0/20"  // Change as per your region/zone
+  network                  = google_compute_network.default.self_link
+  region                   = var.region
+  private_ip_google_access = true
+}
+
+data "google_client_config" "current" {
+}
+
+data "google_container_engine_versions" "default" {
+  location = var.location
+}
+
+resource "google_container_cluster" "default" {
+  name               = var.network_name
+  location           = var.location
+  initial_node_count = 3
+  min_master_version = data.google_container_engine_versions.default.latest_master_version
+  network            = google_compute_subnetwork.default.name
+  subnetwork         = google_compute_subnetwork.default.name
+
+  remove_default_node_pool = true
+  enable_legacy_abac = true
+
+  // Wait for the GCE LB controller to cleanup the resources.
+  // Wait for the GCE LB controller to cleanup the resources.
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 90"
+  }
+
+  provider = google-beta
+  
+  cluster_autoscaling {
+    enabled = true
+    autoscaling_profile = "OPTIMIZE_UTILIZATION"
+    resource_limits {
+      resource_type = "cpu"
+      minimum = 2
+      maximum = 8
+    }
+    resource_limits {
+      resource_type = "memory"
+      minimum = 8
+      maximum = 16
+    }
+  }
+
+  vertical_pod_autoscaling {
+    enabled = true
+  }
+}
+
+
+output "network" {
+  value = google_compute_subnetwork.default.network
+}
+
+output "subnetwork_name" {
+  value = google_compute_subnetwork.default.name
+}
+
+output "cluster_name" {
+  value = google_container_cluster.default.name
+}
+
+output "cluster_region" {
+  value = var.region
+}
+
+output "cluster_location" {
+  value = google_container_cluster.default.location
+}
