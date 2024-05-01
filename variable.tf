3bf10ba93ce7c21028560e7741ec3b28d6dd970c
loner021007 <150621153+loner021007@users.noreply.github.com>
2024-05-01 07:36:52 +0000

    changes

--- variable.tf
+++ variable.tf
@@ -28,19 +28,19 @@ variable private_subnet2_cidr {
   default = "10.0.8.0/22"
 }
 
-variable "database_name" {
+variable database_name {
   description = "Name of the database"
   type        = string
   default     = "MyDatabase"
 }
 
-variable "master_username" {
+variable master_username {
   description = "Username for the master user"
   type        = string
   default     = "admin"
 }
 
-variable "manage_master_user_password" {
+variable manage_master_user_password {
   description = "Password for the master user"
   type        = string
   default     = "admin123"
