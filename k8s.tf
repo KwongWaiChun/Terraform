408b9e176b665bba4a0c2eb107a3c7cc38213302
loner021007 <150621153+loner021007@users.noreply.github.com>
2024-05-11 17:24:06 +0000

    changes

--- k8s.tf
+++ k8s.tf
@@ -1,3 +1,15 @@
+provider "kubernetes" {
+  version = "~> 1.10.0"
+  host    = google_container_cluster.default.endpoint
+  token   = data.google_client_config.current.access_token
+  client_certificate = base64decode(
+    google_container_cluster.default.master_auth[0].client_certificate,
+  )
+  client_key = base64decode(google_container_cluster.default.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+  )
+}
 
 resource "kubernetes_namespace" "staging" {
   metadata {
